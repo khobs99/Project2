2a1e2b18db62865d9017dfc37ffa9377d9379373
khobs99 <khobday99@gmail.com>
2020-02-06 17:55:58 -0500

    Create People.cpp

---
+++ People.cpp
@@ -0,0 +1,28 @@
+#ifndef Person_KJH
+#define Person_KJH
+
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+#include<string>
+#include<iomanip>
+
+
+
+using namespace std;
+
+
+#include "People.h"
+
+people::people()
+{
+
+}
+people::people()
+{
+
+}
+
+
+
+#endif
