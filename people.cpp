4b5f4116ffaa5d1d7c09db125ff041c91893127f
khobs99 <khobday99@gmail.com>
2020-02-08 22:52:25 -0500

    Update people.cpp

--- people.cpp
+++ people.cpp
@@ -17,22 +17,77 @@ using namespace std;
 
 people::people()
 {
-	persn = new string[20];
+	map = new person[20];
+	len = 0;
 }
 people::people(const people& source)
 {
-
+	source = new person[20];
+	len = source.len;
+	*this = source;
 }
 people::~people()
 {
-	delete[] persn;
+	delete[] map;
 }
-bool people::insert(const person& x)
+void people::insert(const person& x)
 {
+	int i, pos = 0;
+	if (len >= 20)
+	{
+		return;
+	}
+	while (pos < len && map[pos] < x)
+	{
+		pos++;
+	}
+	for (i = len; i > pos; i--)
+	{
+		map[i - 1] = map[i];
+		map[pos] = x;
+		len++;
+	}
 
 }
-bool people::operator + (people l)
+void people::display(ostream& out)
 {
+	out << map << endl;
+}
+int people::find(const person& f)
+{
+	for (int i = 0; i < len; i++)
+	{
+		if (map[i] == f)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+bool people::remove(person& j)
+{
+	delete map[j];
+}
+people people::operator + (people l)
+{
+	people r(*this);
+	for (int i = 0; i < len; i++)
+	{
+		if (l.find(map[i]) != -1)
+		{
+			r.insert(map[i]);
+		}
+	}
+	return r;
+}
+void people::operator =(people& i)
+{
+	person *tempmap;
+	tempmap = new person[20];
+	delete[] map;
+	map = tempmap;
+	i.map = tempmap;
+	len = i.len;
 	
 }
 
