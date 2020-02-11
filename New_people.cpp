c2cbcf93f8cd0f7df29c37611f3985e8b8763fd5
khobs99 <khobday99@gmail.com>
2020-02-10 22:20:24 -0500

    Create New_people.cpp

---
+++ New_people.cpp
@@ -0,0 +1,54 @@
+#ifndef Person_KJH
+#define Person_KJH
+
+#include<iomanip>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+
+
+
+#include"people.h"
+#include "Person_KJH.h"
+
+
+people::people()
+{
+	map = new person[20];
+	len = 0;
+}
+people::~people()
+{
+	delete[] map;
+}
+people::people(const people& source)
+{
+	source = new person[20];   //error no operator matches these operands
+	len = source.len;
+	*this = source;
+}
+
+people& people::operator= (const people &source)
+{
+	if (this == &source)
+	{
+		return *this;
+	}
+	len = source.len;
+	map = source.map;
+	return *this;
+	
+}
+void people::display(ostream& out)
+{
+	out << map<<endl;
+}
+
+
+
+
+
+
+
+#endif
